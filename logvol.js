a5447110cbca3a7b221de515b8a1d8badc633429
trilif <68926406+trilif@users.noreply.github.com>
2022-12-25 18:14:59 +0100

    Update logvol.js

--- logvol.js
+++ logvol.js
@@ -14,11 +14,9 @@ client.connect();
 
 app.get('/:a/:length/:d', (req, res)=>{
 	res.header('Access-Control-Allow-Origin', "*");
-	//next lines against SQL Injection!
 	const FF = ["T","CS","B","A","GYEKL","NY","ELL","F"];
 	if(FF.includes(req.params.a)&&req.params.d>=12&&req.params.d<=100&&req.params.length>=10&&req.params.length<=60){
-    client.query('SELECT l'+req.params.length+' as vol FROM '+req.params.a+' WHERE d=' +req.params.d, (err, result)=>{
-		
+    client.query('SELECT l'+req.params.length+' as vol FROM '+req.params.a+' WHERE d=' +req.params.d, (err, result)=>{	
         if(!err){
             res.send(''+result.rows[0].vol);
         }
